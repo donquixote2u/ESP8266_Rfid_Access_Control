e092bc17e76bbbd1de823dd50d5ae913148f0d13
donquixote2u <donquixote2u@gmail.com>
2018-11-13 18:36:40 +1300

    modified:   ESP8266_Rfid_Access_Control.ino

--- ESP8266_Rfid_Access_Control.ino
+++ ESP8266_Rfid_Access_Control.ino
@@ -1,10 +1,10 @@
 /* if RF reader detects an authorised tag, fire door control solenoid
  *  RF reader outputs ID of tag read onto serial port
- * ESP8266 uses D1,D2  for rf reader soft serial port rx and debug tx, 
+ * ESP8266 uses D7,D8  for rf reader alt serial port rx and debug tx, 
  *  and D3 for digital LOW alert
  */ 
-#define RX1 5 // *** D1 = Rx = GPIO5
-#define TX1 4 // *** D2 =  DEBUG OUTPUT = GPIO4
+// *** D7 = Rx = GPIO13
+// *** D8 =  DEBUG OUTPUT = GPIO15
 #define ALERT 0  // digital out pin for threshold alert = D3 = GPIO0
 #define DELAY 100    // delay in millisecs between cap tests
 #define SensorRate 9600
@@ -12,8 +12,6 @@
 #define tag1 1402397
 #define tag2 13535691
 // #include <Wifi_Credentials.h>
-#include <SoftwareSerial.h>
-SoftwareSerial RFin(RX1,TX1);
 
 char rxbuffer[INDEX_SIZE] = {}; //  receive buffer
 unsigned long ID=0;                   // 
@@ -21,39 +19,39 @@ int buffptr = 0; // position in circular buffer above
 
 void setup()                    
 {
-   RFin.begin(SensorRate);
    Serial.begin(SensorRate);
+   Serial.swap();                 // reassign serial uart 0 to GPIO15,13
    pinMode(ALERT,OUTPUT);         // pulled low if tag detected
    digitalWrite(ALERT, HIGH);
 }
 
 void loop()                    
 {
- if(RFin.available()>0)
+ if(Serial.available()>0)
    {
    buffptr=0; ID=0;
-   while(RFin.available()>0)
+   while(Serial.available()>0)
        {
-        rxbuffer[buffptr]=RFin.read();
+        rxbuffer[buffptr]=Serial.read();
         buffptr++;
        }              // end while serial
    for(int i=0;i<buffptr;i++)
       {     
-       RFin.write(rxbuffer[i]);
        Serial.write(rxbuffer[i]);
        ID <<= 8;      // shift left 8 bits, add new byte to ID number
        ID |= rxbuffer[i];
       }
-   RFin.write('-'); 
-   RFin.print(ID,DEC);
-   RFin.write('\n');  
+   Serial.write('-'); 
+   Serial.print(ID,DEC);
+   Serial.write('\n');  
    if(ID==tag1 || ID==tag2)  // if authoried tag detected, 
       {
       digitalWrite(ALERT,LOW);
       delay(200);
       digitalWrite(ALERT,HIGH);
-      RFin.print("tag read\n"); 
+      Serial.print("tag read\n"); 
       }
+   else { Serial.print("\nunidentified tag"); }   
    }
 delay(200);     
 }
